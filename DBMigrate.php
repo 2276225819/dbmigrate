dbd69a9927f3a472d940f1bc60a8d54b79f6d612
2276225819 <2276225819@qq.com>
2016-10-31 22:54:29 +0800

    add DBMigrate::engine

--- DBMigrate.php
+++ DBMigrate.php
@@ -1,21 +1,23 @@
 <?php 
-if(!function_exists('array_column')){
+if(!function_exists('array_column')){ 
     function array_column($arr,$col){  
         return array_map(function($v)use($col){return $v[$col];},$arr);
-    }
+    } 
 } 
 class DBMigrate {
-    private $pdo;
-    private $exts;
-    private $keys;
-    private $column; 
-    private $prefix;
-    private $ischeck=false;
+    public $pdo;
+    public $exts;
+    public $keys;
+    public $tables; 
+    public $prefix;
+    public $engine;
+    public $comment='\'DBMigrate\'';
+    public $ischeck=false;
     public function __construct($pdo,$config=array()){ 
         if( empty($pdo) || !($pdo instanceof \PDO) )
-            throw new Exception("DBMigrate Error: PDO Request", 1);
+            throw new Exception("DBMigrate Error: 'PDO Request'", 1);
         $this->pdo = $pdo;
-        $this->prefix = isset($config['prefix'])?$config['prefix']:'';  
+        $this->prefix = isset($config['prefix'])?$config['prefix']:'';   
         $this->keys  = array(
             'int'=> 'int',
             'integer'=> 'int',
@@ -38,29 +40,36 @@ class DBMigrate {
             'comment'=> 'comment',
             'default'=> 'default',
         );
-        $this->column = array(); 
+        $this->tables = array(
 
+        );  
         if(isset($config['column']))
-            $this->column = array_merge($this->column, $config['column']);  
+            $this->tables = array_merge($this->tables, $config['column']);  
         if(isset($config['keys']))
             $this->keys = array_merge($this->keys, $config['keys']); 
         if(isset($config['exts']))
-            $this->exts = array_merge($this->exts,$config['exts']); 
+            $this->exts = array_merge($this->exts,$config['exts']);  
+        if(isset($config['engine']))
+            $this->engine($this->engine = $config['engine']);
     }
 
 
-    public $last_table='_';
+    public $last_table='_'; 
+    public $last_index=0;
     public function __call($name,$args){   
         //laravel  
         if($name=='table'){
             $this->last_table=$args[0]; 
+            $this->last_index=0;
+            $this->tables[$args[0]]['engine']=$this->engine;
+            $this->tables[$args[0]]['comment']=$this->comment;
             if(isset($args[1])){  
                 if(is_callable($args[1])) $args[1]($this);
                 else{
-                    if(empty($this->column[$args[0]]))
-                        $this->column[$args[0]]=array();
-                    $this->column[$args[0]] = array_merge(
-                        $this->column[$args[0]], 
+                    if(empty($this->tables[$args[0]]))
+                        $this->tables[$args[0]]=array();
+                    $this->tables[$args[0]] = array_merge(
+                        $this->tables[$args[0]], 
                         array_filter(preg_split("/[,\r\n\t][ \r\n\t]+/",$args[1]))
                     );  
                 } 
@@ -73,20 +82,40 @@ class DBMigrate {
         } 
         if($name == 'increment' || $name =='increments'){   
             $str = array_shift($args)." int auto_increment primary key";
-            $this->column[$ntable][]=$str; 
+            $this->tables[$ntable][]=$str; 
+            $this->last_index++;
             return $this;
         }      
         if( isset( $this->keys[$name] ) ){ 
             $str = array_shift($args).' '.$this->keys[$name];
             if($args) $str.='('.join($args,',').')';
-            $this->column[$ntable][]=$str; 
+            $this->tables[$ntable][]=$str; 
+            $this->last_index++;
             return $this;
         }   
-        if( isset( $this->exts[$name] )){
-            $index = count($this->column[$ntable])-1; 
-            $this->column[$ntable][$index] .= " {$this->exts[$name]} ".var_export($args[0],true); 
+        if( isset( $this->exts[$name] )){ 
+            if($this->last_index==0){ 
+                $this->tables[$ntable][$this->exts[$name]]=var_export($args[0],true);
+            }else{
+                $this->tables[$ntable][$this->last_index-1] .= " {$this->exts[$name]} ".var_export($args[0],true);  
+            }
             return $this;
         }
+    }
+    public function engine($str){   
+        static $engines;
+        if(empty($engines)){ 
+            $engines = array_column($this->_query("show engines"),0); 
+        }   
+        foreach ($engines as $type) { 
+            if( stristr($type,$str) && strlen($type) === strlen($str) ){  
+                if($this->last_table!='_'){ 
+                    $this->tables[$this->last_table]['engine'] = $type; 
+                }
+                return $this;
+            }
+        }  
+        throw new Exception("DBMigrate Error: 'Not support engine type ({$str})'", 1); 
     }
     public function check(){
         $this->ischeck=true;
@@ -95,9 +124,8 @@ class DBMigrate {
     public function clean($before=null){
         if(!empty($before) && is_callable($before))
             $before($this);   
-        foreach ($this->column as $table => $col) { 
-            if($table=='_')
-                continue;
+        foreach ($this->tables as $table => $col) { 
+            if($table=='_') continue;
             $col = array_map(function($v){
                 list($n)=explode(' ',$v);
                 return $n;
@@ -116,7 +144,7 @@ class DBMigrate {
         if(!empty($before) && is_callable($before))
             $before($this);    
         $tables = array_column($this->_query('show tables'),0);  
-        foreach ($this->column as $table => $col) {
+        foreach ($this->tables as $table => $col) {
             if($table=='_')continue;
             if(in_array($this->prefix.$table,$tables))
                 $this->_alterTable($table,$col); 
@@ -128,16 +156,15 @@ class DBMigrate {
     public function truncate($before=null){
         if(!empty($before) && is_callable($before))
             $before($this);   
-        foreach ($this->column as $table => $col) { 
-            if($table=='_')
-                continue;
+        foreach ($this->tables as $table => $col) { 
+            if($table=='_')  continue;
             $this->_exec("truncate $table");
         } 
         return $this; 
     }
  
     public function export(){
-        return var_export($this->column,true); 
+        return var_export($this->tables,true); 
     }
    
     function _query($sql,$args=array()){
@@ -166,21 +193,36 @@ class DBMigrate {
     } 
     function _createTable($table,$column ){   
         $str = "create table {$table}(\n   ";
-        $_tag='';
+        $ext="";
+        $_tag1=$_tag2='';
         foreach ($column as $key =>$value){ 
-            $str.= $_tag.$value;//arr[1];
-            if(empty($_tag)) $_tag=",\n   ";  
+            if(!is_numeric($key)){
+                $ext.="$_tag1 $key=$value "; 
+                if(empty($_tag1)) $_tag1=",  "; 
+            }else{
+                $str.= $_tag2.$value;//arr[1];
+                if(empty($_tag2)) $_tag2=",\n   ";   
+            }
         }
-        $str.="\n);";    
+        $str.="\n) $ext ;";    
         return $this->_exec($str,array(),$this->ischeck);  
     }
     function _alterTable($table,$column ){   
         $result = $this->_query("show columns from $table");
+        $format = $this->_query("show create table $table");
+        $format = $format[0]["Create Table"];//
         $tps = array_column($result ,1); 
         $fns = array_column($result ,0);
         $fns = array_flip($fns);
 
-        foreach ($column as $value){ 
+        foreach ($column as $key => $value){ 
+            if(!is_numeric($key)){  
+                if(stristr($format,"$key=$value")===false){
+                    $str = "alter table {$table} $key $value;";   
+                    $this->_exec($str,array(),$this->ischeck);   
+                } 
+                continue;
+            }
             list($name,$type) = explode(' ',$value);   
             if( isset($fns[$name]) ){  
                 if( !strstr($tps[$fns[$name]],$type) ){ 
@@ -193,4 +235,5 @@ class DBMigrate {
             } 
         }  
     } 
+ 
 }
--- example.php
+++ example.php
@@ -1,20 +1,18 @@
 <?php 
 include "DBMigrate.php";
-
-
+ 
 $conn = 'mysql:charset=utf8'.';dbname=test'; 
 $pdo  =  new \PDO($conn,'root','root',array( ));   
-
-
-$obj = (new DBMigrate($pdo))->sync(function($DBMigrate){
+$obj = (new DBMigrate($pdo,['engine'=>'innodb']))->sync(function($DBMigrate){
      
-    $DBMigrate
-    ->table('user')
+
+    $DBMigrate//->check()
+    ->table('user') ->comment('用户表') 
         ->increment('id')
         ->varchar('username',32)
-        ->varchar('password',32)
             ->comment('md5')
-    ->table('post')
+        ->varchar('password',32)
+    ->table('post') ->engine('myisam')
         ->increment('id')
         ->varchar('title',32)
         ->text('comment')
@@ -25,8 +23,11 @@ $obj = (new DBMigrate($pdo))->sync(function($DBMigrate){
         title varchar(32)  ,
         ip varchar(12)  ,  
     ")->int('num'); 
+
+ 
 });
 
 
 print_r($obj->log);
+print_r($obj->tables);
 echo 'END';
